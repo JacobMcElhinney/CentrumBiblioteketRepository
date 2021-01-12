55d20d9a13539f1d879b452cb5354ae99bfd46cc
Jacob McElhinney <jacob.mcelhinney@outlook.com>
2021-01-12 18:47:48 +0100

    New Database. New  Initial Migration

--- CentrumBiblioteket/Models/BookEdition.cs
+++ CentrumBiblioteket/Models/BookEdition.cs
@@ -25,10 +25,5 @@ namespace CentrumBiblioteket.Models
         //Used to facilitate cardinal relationship: many-to-many.
         public virtual ICollection<BookEdition_Author> BookEdition_Authors { get; set; }
 
-        //One BookEdition may have many BookCopies.
-        public virtual ICollection<BookCopy> BookCopies { get; set; }
-
-        //One-to-many: One BookEdition may be registered many times in the BookLoans table.
-        public virtual ICollection<BookLoan> BookLoans { get; set; }
     }
 }
