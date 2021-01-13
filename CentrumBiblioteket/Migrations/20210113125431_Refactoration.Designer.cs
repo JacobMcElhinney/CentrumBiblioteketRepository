ba59eee3d189e95c578eee63b4ca79e3a47728e9
Jacob McElhinney <jacob.mcelhinney@outlook.com>
2021-01-13 13:55:25 +0100

    New Migration

--- CentrumBiblioteket/Migrations/20210113125431_Refactoration.Designer.cs
+++ CentrumBiblioteket/Migrations/20210113125431_Refactoration.Designer.cs
@@ -4,14 +4,16 @@ using CentrumBiblioteket.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CentrumBiblioteket.Migrations
 {
     [DbContext(typeof(CentrumBiblioteketDbContext))]
-    partial class CentrumBiblioteketDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210113125431_Refactoration")]
+    partial class Refactoration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CentrumBiblioteket/Migrations/20210113125431_Refactoration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CentrumBiblioteket.Migrations
+{
+    public partial class Refactoration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookLoans_BookEditions_BookEditionId",
+                table: "BookLoans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BookLoans_BookEditionId",
+                table: "BookLoans");
+
+            migrationBuilder.DropColumn(
+                name: "BookEditionId",
+                table: "BookLoans");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BookEditionId",
+                table: "BookLoans",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookLoans_BookEditionId",
+                table: "BookLoans",
+                column: "BookEditionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookLoans_BookEditions_BookEditionId",
+                table: "BookLoans",
+                column: "BookEditionId",
+                principalTable: "BookEditions",
+                principalColumn: "BookEditionId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
